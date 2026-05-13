f19d1edf4998811b8476230c308792fd7dd91116
olga-kuzn <kuzn.olga@gmail.com>
2026-05-13 15:58:08 +0200

    Second commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://openaiapp-ok.streamlit.app/
--- app.py
+++ app.py
@@ -7,3 +7,18 @@ from dotenv import load_dotenv
 load_dotenv('.env')
 OpenAI_key : str = os.getenv('OPENAI_API_KEY')
 
+from openai import OpenAI
+
+MODEL="gpt-4o-mini"
+client = OpenAI()
+
+richiesta = st.text_input('inserisci richiesta ')
+completion = client.chat.completions.create(
+                                            model=MODEL,
+                                            messages=[
+                                                {"role": "system", "content": "Sei un assistente per lo studio. Cortesemente aiutami con i compiti"},
+                                                {"role": "user", "content": richiesta}
+                                            ]
+                                            )
+
+st.write("Assistente: " + completion.choices[0].message.content)
